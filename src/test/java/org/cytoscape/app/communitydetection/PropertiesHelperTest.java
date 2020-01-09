713542247416ac2a3d20ad931484ae6c4513ac10
Chris Churas <churas.camera@gmail.com>
2020-01-08 17:10:13 -0800

    initial revision

---
+++ src/test/java/org/cytoscape/app/communitydetection/PropertiesHelperTest.java
@@ -0,0 +1,16 @@
+package org.cytoscape.app.communitydetection;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ *
+ * @author churas
+ */
+public class PropertiesHelperTest {
+    
+    @Test
+    public void testfoo(){
+	assertEquals(1, 1);
+    }
+}
