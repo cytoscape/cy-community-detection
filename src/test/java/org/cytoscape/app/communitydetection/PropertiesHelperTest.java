69c1f430e35aa8b97af51381354be58d0b09a653
Chris Churas <churas.camera@gmail.com>
2020-01-27 10:27:18 -0800

    added more tests

--- src/test/java/org/cytoscape/app/communitydetection/PropertiesHelperTest.java
+++ src/test/java/org/cytoscape/app/communitydetection/PropertiesHelperTest.java
@@ -14,9 +14,31 @@ public class PropertiesHelperTest {
 	PropertiesHelper ph = PropertiesHelper.getInstance();
 	assertNull(ph.getBaseurl());
 	assertNull(ph.getThreadcount());
+	assertEquals(0, ph.getHttpSocketTimeoutMillis());
+	assertEquals(0, ph.getHttpConnectTimeoutMillis());
+	assertEquals(0, ph.getHttpConnectionRequestTimeoutMillis());
+	assertEquals(0, ph.getPollingMaxRetryCount());
+	assertEquals(0, ph.getSubmitRetryCount());
+	assertEquals(0, ph.getCommunityDetectionTimeoutMillis());
+	assertEquals(0, ph.getFunctionalEnrichmentTimeoutMillis());
+			
 	ph.setBaseurl("url");
 	ph.setThreadcount("thread");
+	ph.setHttpSocketTimeoutMillis(1);
+	ph.setHttpConnectTimeoutMillis(2);
+	ph.setHttpConnectionRequestTimeoutMillis(3);
+	ph.setPollingMaxRetryCount(4);
+	ph.setSubmitRetryCount(5);
+	ph.setCommunityDetectionTimeoutMillis(6);
+	ph.setFunctionalEnrichmentTimeoutMillis(7);
 	assertEquals("url", ph.getBaseurl());
 	assertEquals("thread", ph.getThreadcount());
+	assertEquals(1, ph.getHttpSocketTimeoutMillis());
+	assertEquals(2, ph.getHttpConnectTimeoutMillis());
+	assertEquals(3, ph.getHttpConnectionRequestTimeoutMillis());
+	assertEquals(4, ph.getPollingMaxRetryCount());
+	assertEquals(5, ph.getSubmitRetryCount());
+	assertEquals(6, ph.getCommunityDetectionTimeoutMillis());
+	assertEquals(7, ph.getFunctionalEnrichmentTimeoutMillis());
     }
 }
