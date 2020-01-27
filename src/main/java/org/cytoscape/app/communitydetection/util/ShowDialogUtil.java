1a05e7a0858cbb2ea40ef8a368a7b8214d830ac4
Chris Churas <churas.camera@gmail.com>
2020-01-27 10:26:33 -0800

    initial revision

---
+++ src/main/java/org/cytoscape/app/communitydetection/util/ShowDialogUtil.java
@@ -0,0 +1,18 @@
+package org.cytoscape.app.communitydetection.util;
+
+import java.awt.Component;
+import javax.swing.Icon;
+import javax.swing.JOptionPane;
+
+/**
+ * 
+ * @author churas
+ */
+public class ShowDialogUtil {
+	
+	
+	public void showMessageDialog(Component parentComponent,
+			Object message, String title, int messageType, Icon icon){
+		JOptionPane.showMessageDialog(parentComponent, message, title, messageType, icon);
+	}
+}
---
+++ src/test/java/org/cytoscape/app/communitydetection/AboutTaskFactoryImplTest.java
@@ -0,0 +1,48 @@
+package org.cytoscape.app.communitydetection;
+
+import javax.swing.ImageIcon;
+import javax.swing.JEditorPane;
+import javax.swing.JOptionPane;
+import org.cytoscape.app.communitydetection.hierarchy.JEditorPaneFactory;
+import org.cytoscape.app.communitydetection.util.AppUtils;
+import org.cytoscape.app.communitydetection.util.ShowDialogUtil;
+import org.cytoscape.application.swing.CySwingApplication;
+import org.cytoscape.model.CyNetwork;
+import static org.mockito.Mockito.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+/**
+ *
+ * @author churas
+ */
+public class AboutTaskFactoryImplTest {
+	
+	@Test
+	public void testCreateTaskIterator(){
+		CySwingApplication mockApp = mock(CySwingApplication.class);
+		when(mockApp.getJFrame()).thenReturn(null);
+		JEditorPaneFactory mockPaneFac = mock(JEditorPaneFactory.class);
+		JEditorPane mockEditorPane = mock(JEditorPane.class);
+		ShowDialogUtil mockDialog = mock(ShowDialogUtil.class);
+		when(mockPaneFac.getDescriptionFrame(any(String.class))).thenReturn(mockEditorPane);
+		
+		AboutTaskFactoryImpl tFac = new AboutTaskFactoryImpl(mockApp, mockPaneFac,
+															mockDialog);
+		tFac.createTaskIterator(null);
+		verify(mockDialog).showMessageDialog(eq(null), eq(mockEditorPane),
+				eq(AppUtils.APP_NAME), eq(JOptionPane.INFORMATION_MESSAGE), any(ImageIcon.class));
+		tFac.createTaskIterator(null);
+	}
+	
+	@Test
+	public void testIsReady(){
+		AboutTaskFactoryImpl tFac = new AboutTaskFactoryImpl(null, null,
+															null);
+		assertTrue(tFac.isReady(null));
+		CyNetwork mockNet = mock(CyNetwork.class);
+		assertTrue(tFac.isReady(mockNet));
+	}
+	
+}
