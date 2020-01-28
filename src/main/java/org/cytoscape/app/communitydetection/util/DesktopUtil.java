9f2bf91a6684a8a6c26b52f141bbcfe58ad1897b
Chris Churas <churas.camera@gmail.com>
2020-01-28 14:12:26 -0800

    initial revision

---
+++ src/main/java/org/cytoscape/app/communitydetection/util/DesktopUtil.java
@@ -0,0 +1,14 @@
+package org.cytoscape.app.communitydetection.util;
+
+import java.awt.Desktop;
+
+/**
+ *
+ * @author churas
+ */
+public class DesktopUtil {
+	
+	public Desktop getDesktop(){
+		return Desktop.getDesktop();
+	}
+}
