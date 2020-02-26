e37d112e52159e8bd969fc2c433d98a479e362e8
Chris Churas <churas.camera@gmail.com>
2020-02-26 12:11:04 -0800

    added new methods needed for HierarchyTaskFactoryImpl

--- src/main/java/org/cytoscape/app/communitydetection/util/ShowDialogUtil.java
+++ src/main/java/org/cytoscape/app/communitydetection/util/ShowDialogUtil.java
@@ -13,10 +13,24 @@ public class ShowDialogUtil {
 	
 	public void showMessageDialog(Component parentComponent,
 			Object message, String title, int messageType, Icon icon){
-		JOptionPane.showMessageDialog(parentComponent, message, title, messageType, icon);
+		JOptionPane.showMessageDialog(parentComponent, message, title,
+				messageType, icon);
 	}
 	
 	public void showMessageDialog(Component parentComponent, Object message){
 		JOptionPane.showMessageDialog(parentComponent, message);
+	}
+	
+	public void showMessageDialog(Component parentComponent, Object message,
+			String title, int messageType){
+		JOptionPane.showMessageDialog(parentComponent, message, title,
+				messageType);
+	}
+	
+	public int showOptionDialog(Component parentComponent, Object message,
+			String title, int optionType, int messageType, Icon icon,
+			Object[] options, Object initialValue){
+		return JOptionPane.showOptionDialog(parentComponent, message,
+				title, optionType, messageType, icon, options, initialValue);
 	}
 }
