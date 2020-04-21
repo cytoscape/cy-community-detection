28bcf83337403918a7b031f307a98f20b951aa7a
Chris Churas <churas.camera@gmail.com>
2020-04-21 11:35:31 -0500

    initial revision

---
+++ src/test/java/org/cytoscape/app/communitydetection/util/JEditorPaneFactoryImplTest.java
@@ -0,0 +1,26 @@
+package org.cytoscape.app.communitydetection.util;
+
+import javax.swing.JEditorPane;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+
+/**
+ *
+ * @author churas
+ */
+public class JEditorPaneFactoryImplTest {
+	
+	@Test
+	public void testGetDescriptionFrame(){
+		JEditorPaneFactoryImpl fac = new JEditorPaneFactoryImpl();
+		JEditorPane jpane = fac.getDescriptionFrame("hello");
+		assertTrue(jpane.getText().contains("hello"));
+		assertTrue(jpane.getText().contains("<html>"));
+		assertTrue(jpane.getText().contains("</html>"));
+		assertFalse(jpane.isEditable());
+		assertFalse(jpane.isOpaque());
+		assertEquals(600, jpane.getPreferredSize().width);
+		
+	}
+}
