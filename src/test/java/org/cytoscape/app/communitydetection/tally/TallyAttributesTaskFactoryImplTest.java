b2ef3115cc9ba0c18c44759782abf6c29d83cef1
Chris Churas <churas.camera@gmail.com>
2021-01-21 11:12:31 -0800

    added tests to verify getParentNetwork method works

--- src/test/java/org/cytoscape/app/communitydetection/tally/TallyAttributesTaskFactoryImplTest.java
+++ src/test/java/org/cytoscape/app/communitydetection/tally/TallyAttributesTaskFactoryImplTest.java
@@ -9,6 +9,7 @@ import org.cytoscape.app.communitydetection.util.AppUtils;
 import org.cytoscape.app.communitydetection.util.CyNetworkUtil;
 import org.cytoscape.app.communitydetection.util.ShowDialogUtil;
 import org.cytoscape.application.swing.CySwingApplication;
+import org.cytoscape.model.CyColumn;
 import org.cytoscape.model.CyNetwork;
 import org.cytoscape.model.CyNetworkManager;
 import org.cytoscape.model.NetworkTestSupport;
@@ -93,6 +94,130 @@ public class TallyAttributesTaskFactoryImplTest {
 		
 	}
 	
+	// test getParentNetwork dialog displayed 0 returned with network to remember
+	@Test
+	public void testGetParentNetworkDisplayDialogReturnZeroAndRememberNetwork() throws Exception {
+		TallyDialog mockTallyDialog = mock(TallyDialog.class);
+		
+		when(mockTallyDialog.getColumnsToTally()).thenReturn(Arrays.asList(mock(CyColumn.class)));
+		when(mockTallyDialog.createGUI(any())).thenReturn(false);
+		ShowDialogUtil mockDialogUtil = mock(ShowDialogUtil.class);
+		when(mockDialogUtil.showOptionDialog(any(), any(), any(),anyInt(),anyInt(),
+				any(), any(), any())).thenReturn(0);
+		CySwingApplication mockSwing = mock(CySwingApplication.class);
+		CyNetworkManager mockNetworkManager = mock(CyNetworkManager.class);
+		when(mockNetworkManager.getNetworkSet()).thenReturn(null);
+		CyNetwork hierarchyNetwork = _nts.getNetwork();
+		_cyNetworkUtil.createTableColumn(hierarchyNetwork.getDefaultNodeTable(),
+				AppUtils.COLUMN_CD_MEMBER_LIST, String.class, false, "");
+		
+		CyNetwork parentNetwork = _nts.getNetwork();
+		_cyNetworkUtil.createTableColumn(parentNetwork.getDefaultNodeTable(),
+				"intcol", Integer.class, false, 0);
+		
+		ParentNetworkFinder mockParentNetworkFinder = mock(ParentNetworkFinder.class);
+		when(mockParentNetworkFinder.findParentNetworks(any(), any())).thenReturn(null);
+		ParentNetworkChooserDialog mockParentNetworkDialog = mock(ParentNetworkChooserDialog.class);
+		when(mockParentNetworkDialog.createGUI(any())).thenReturn(true);
+		when(mockParentNetworkDialog.rememberChoice()).thenReturn(true);
+		
+		CyNetworkUtil mockNetworkUtil = mock(CyNetworkUtil.class);
+		
+		TallyAttributesTaskFactoryImpl tfac = new TallyAttributesTaskFactoryImpl(mockSwing,
+				mockDialogUtil, mockTallyDialog, mockParentNetworkFinder,
+				mockParentNetworkDialog, mockNetworkUtil, mockNetworkManager);
+		
+		TaskIterator ti = tfac.createTaskIterator(hierarchyNetwork);
+		assertTrue(ti.next() instanceof DoNothingTask);
+		verify(mockDialogUtil).showOptionDialog(any(),any(),
+				eq("Parent Network Chooser"), eq(JOptionPane.YES_NO_OPTION),
+				eq(JOptionPane.PLAIN_MESSAGE), any(), any(), any());
+		verify(mockNetworkUtil).updateHierarchySUID(any(), any());
+	}
+
+
+	// test getParentNetwork dialog displayed 0 returned do NOT remember network
+	@Test
+	public void testGetParentNetworkDisplayDialogReturnZeroNoRememberNetwork() throws Exception {
+		TallyDialog mockTallyDialog = mock(TallyDialog.class);
+		
+		when(mockTallyDialog.getColumnsToTally()).thenReturn(Arrays.asList(mock(CyColumn.class)));
+		when(mockTallyDialog.createGUI(any())).thenReturn(false);
+		ShowDialogUtil mockDialogUtil = mock(ShowDialogUtil.class);
+		when(mockDialogUtil.showOptionDialog(any(), any(), any(),anyInt(),anyInt(),
+				any(), any(), any())).thenReturn(0);
+		CySwingApplication mockSwing = mock(CySwingApplication.class);
+		CyNetworkManager mockNetworkManager = mock(CyNetworkManager.class);
+		when(mockNetworkManager.getNetworkSet()).thenReturn(null);
+		CyNetwork hierarchyNetwork = _nts.getNetwork();
+		_cyNetworkUtil.createTableColumn(hierarchyNetwork.getDefaultNodeTable(),
+				AppUtils.COLUMN_CD_MEMBER_LIST, String.class, false, "");
+		
+		CyNetwork parentNetwork = _nts.getNetwork();
+		_cyNetworkUtil.createTableColumn(parentNetwork.getDefaultNodeTable(),
+				"intcol", Integer.class, false, 0);
+		
+		ParentNetworkFinder mockParentNetworkFinder = mock(ParentNetworkFinder.class);
+		when(mockParentNetworkFinder.findParentNetworks(any(), any())).thenReturn(null);
+		ParentNetworkChooserDialog mockParentNetworkDialog = mock(ParentNetworkChooserDialog.class);
+		when(mockParentNetworkDialog.createGUI(any())).thenReturn(true);
+		when(mockParentNetworkDialog.rememberChoice()).thenReturn(false);
+		
+		CyNetworkUtil mockNetworkUtil = mock(CyNetworkUtil.class);
+		
+		TallyAttributesTaskFactoryImpl tfac = new TallyAttributesTaskFactoryImpl(mockSwing,
+				mockDialogUtil, mockTallyDialog, mockParentNetworkFinder,
+				mockParentNetworkDialog, mockNetworkUtil, mockNetworkManager);
+		
+		TaskIterator ti = tfac.createTaskIterator(hierarchyNetwork);
+		assertTrue(ti.next() instanceof DoNothingTask);
+		verify(mockDialogUtil).showOptionDialog(any(),any(),
+				eq("Parent Network Chooser"), eq(JOptionPane.YES_NO_OPTION),
+				eq(JOptionPane.PLAIN_MESSAGE), any(), any(), any());
+		Mockito.verifyNoInteractions(mockNetworkUtil);
+	}
+	
+	// test getParentNetwork dialog displayed non zero returned
+	@Test
+	public void testGetParentNetworkDisplayDialogReturnNonZeroReturned() throws Exception {
+		TallyDialog mockTallyDialog = mock(TallyDialog.class);
+		
+		when(mockTallyDialog.getColumnsToTally()).thenReturn(Arrays.asList(mock(CyColumn.class)));
+		when(mockTallyDialog.createGUI(any())).thenReturn(false);
+		ShowDialogUtil mockDialogUtil = mock(ShowDialogUtil.class);
+		when(mockDialogUtil.showOptionDialog(any(), any(), any(),anyInt(),anyInt(),
+				any(), any(), any())).thenReturn(1);
+		CySwingApplication mockSwing = mock(CySwingApplication.class);
+		CyNetworkManager mockNetworkManager = mock(CyNetworkManager.class);
+		when(mockNetworkManager.getNetworkSet()).thenReturn(null);
+		CyNetwork hierarchyNetwork = _nts.getNetwork();
+		_cyNetworkUtil.createTableColumn(hierarchyNetwork.getDefaultNodeTable(),
+				AppUtils.COLUMN_CD_MEMBER_LIST, String.class, false, "");
+		
+		CyNetwork parentNetwork = _nts.getNetwork();
+		_cyNetworkUtil.createTableColumn(parentNetwork.getDefaultNodeTable(),
+				"intcol", Integer.class, false, 0);
+		
+		ParentNetworkFinder mockParentNetworkFinder = mock(ParentNetworkFinder.class);
+		when(mockParentNetworkFinder.findParentNetworks(any(), any())).thenReturn(null);
+		ParentNetworkChooserDialog mockParentNetworkDialog = mock(ParentNetworkChooserDialog.class);
+		when(mockParentNetworkDialog.createGUI(any())).thenReturn(true);
+		when(mockParentNetworkDialog.rememberChoice()).thenReturn(false);
+		
+		CyNetworkUtil mockNetworkUtil = mock(CyNetworkUtil.class);
+		
+		TallyAttributesTaskFactoryImpl tfac = new TallyAttributesTaskFactoryImpl(mockSwing,
+				mockDialogUtil, mockTallyDialog, mockParentNetworkFinder,
+				mockParentNetworkDialog, mockNetworkUtil, mockNetworkManager);
+		
+		TaskIterator ti = tfac.createTaskIterator(hierarchyNetwork);
+		assertTrue(ti.next() instanceof DoNothingTask);
+		verify(mockDialogUtil).showOptionDialog(any(),any(),
+				eq("Parent Network Chooser"), eq(JOptionPane.YES_NO_OPTION),
+				eq(JOptionPane.PLAIN_MESSAGE), any(), any(), any());
+		Mockito.verifyNoInteractions(mockNetworkUtil);
+	}
+	
 	@Test
 	public void testCreateTaskIteratorTallyCreateGuiFalse() throws Exception {
 		TallyDialog mockTallyDialog = mock(TallyDialog.class);
@@ -165,4 +290,81 @@ public class TallyAttributesTaskFactoryImplTest {
 		assertTrue(ti.next() instanceof DoNothingTask);
 	}
 	
+	@Test
+	public void testCreateTaskIteratorSuccessExceptNoTallyColumns() throws Exception {
+		TallyDialog mockTallyDialog = mock(TallyDialog.class);
+		when(mockTallyDialog.getColumnsToTally()).thenReturn(null);
+		when(mockTallyDialog.createGUI(any())).thenReturn(true);
+		ShowDialogUtil mockDialogUtil = mock(ShowDialogUtil.class);
+		when(mockDialogUtil.showOptionDialog(any(), any(), any(),anyInt(),anyInt(),
+				any(), any(), any())).thenReturn(0);
+		CySwingApplication mockSwing = mock(CySwingApplication.class);
+		CyNetworkManager mockNetworkManager = mock(CyNetworkManager.class);
+		when(mockNetworkManager.getNetworkSet()).thenReturn(null);
+		CyNetwork hierarchyNetwork = _nts.getNetwork();
+		_cyNetworkUtil.createTableColumn(hierarchyNetwork.getDefaultNodeTable(),
+				AppUtils.COLUMN_CD_MEMBER_LIST, String.class, false, "");
+		
+		CyNetwork parentNetwork = _nts.getNetwork();
+		_cyNetworkUtil.createTableColumn(hierarchyNetwork.getDefaultNodeTable(),
+				AppUtils.COLUMN_CD_MEMBER_LIST, String.class, false, "");
+		_cyNetworkUtil.createTableColumn(hierarchyNetwork.getDefaultNodeTable(),
+				"intcol", Integer.class, false, 0);
+		_cyNetworkUtil.createTableColumn(hierarchyNetwork.getDefaultNodeTable(),
+				"doublecol", Double.class, false, 0.0);
+		_cyNetworkUtil.createTableColumn(hierarchyNetwork.getDefaultNodeTable(),
+				"doublecol", Boolean.class, false, false);
+		
+		ParentNetworkFinder mockParentNetworkFinder = mock(ParentNetworkFinder.class);
+		when(mockParentNetworkFinder.findParentNetworks(any(), any())).thenReturn(Arrays.asList(parentNetwork));
+		ParentNetworkChooserDialog mockParentNetworkDialog = mock(ParentNetworkChooserDialog.class);			
+		
+		TallyAttributesTaskFactoryImpl tfac = new TallyAttributesTaskFactoryImpl(mockSwing,
+				mockDialogUtil, mockTallyDialog, mockParentNetworkFinder,
+				mockParentNetworkDialog, null, mockNetworkManager);
+		
+		TaskIterator ti = tfac.createTaskIterator(hierarchyNetwork);
+		assertTrue(ti.next() instanceof DoNothingTask);
+		verify(mockDialogUtil).showMessageDialog(any(),
+				eq("No columns selected to tally"),
+				eq(AppUtils.APP_NAME), eq(JOptionPane.ERROR_MESSAGE));
+	}
+	
+	@Test
+	public void testCreateTaskIteratorSuccess() throws Exception {
+		TallyDialog mockTallyDialog = mock(TallyDialog.class);
+		
+		when(mockTallyDialog.getColumnsToTally()).thenReturn(Arrays.asList(mock(CyColumn.class)));
+		when(mockTallyDialog.createGUI(any())).thenReturn(true);
+		ShowDialogUtil mockDialogUtil = mock(ShowDialogUtil.class);
+		when(mockDialogUtil.showOptionDialog(any(), any(), any(),anyInt(),anyInt(),
+				any(), any(), any())).thenReturn(0);
+		CySwingApplication mockSwing = mock(CySwingApplication.class);
+		CyNetworkManager mockNetworkManager = mock(CyNetworkManager.class);
+		when(mockNetworkManager.getNetworkSet()).thenReturn(null);
+		CyNetwork hierarchyNetwork = _nts.getNetwork();
+		_cyNetworkUtil.createTableColumn(hierarchyNetwork.getDefaultNodeTable(),
+				AppUtils.COLUMN_CD_MEMBER_LIST, String.class, false, "");
+		
+		CyNetwork parentNetwork = _nts.getNetwork();
+		_cyNetworkUtil.createTableColumn(parentNetwork.getDefaultNodeTable(),
+				AppUtils.COLUMN_CD_MEMBER_LIST, String.class, false, "");
+		_cyNetworkUtil.createTableColumn(parentNetwork.getDefaultNodeTable(),
+				"intcol", Integer.class, false, 0);
+		_cyNetworkUtil.createTableColumn(parentNetwork.getDefaultNodeTable(),
+				"doublecol", Double.class, false, 0.0);
+		_cyNetworkUtil.createTableColumn(parentNetwork.getDefaultNodeTable(),
+				"boolcol", Boolean.class, false, false);
+		
+		ParentNetworkFinder mockParentNetworkFinder = mock(ParentNetworkFinder.class);
+		when(mockParentNetworkFinder.findParentNetworks(any(), any())).thenReturn(Arrays.asList(parentNetwork));
+		ParentNetworkChooserDialog mockParentNetworkDialog = mock(ParentNetworkChooserDialog.class);			
+		
+		TallyAttributesTaskFactoryImpl tfac = new TallyAttributesTaskFactoryImpl(mockSwing,
+				mockDialogUtil, mockTallyDialog, mockParentNetworkFinder,
+				mockParentNetworkDialog, null, mockNetworkManager);
+		
+		TaskIterator ti = tfac.createTaskIterator(hierarchyNetwork);
+		assertTrue(ti.next() instanceof TallyTask);
+	}
 }
